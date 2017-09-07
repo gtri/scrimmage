0bdb8aeef35f61528987b296d0416aa3df5644ca
Kenny Bowers <kenneth.bowers@gtri.gatech.edu>
2017-09-07 09:17:07 -0400

    Add Parameter Optimization Tool
    Stubbing out parameter optimizer
    
    Simple Mako working
    
    Generate mission file given parameter labels and values
    
    Better parameter error handling, and generate mission files from LHS
    
    run scrimmage mission
    
    Run and wait multiple missions, parsing log path
    
    post scrimmage analysis, now has full list of input and output values. Ready for the f_approx.
    
    Bayesian Optimization example
    
    Bayesian Optimization Demo
    
    Added GP to analysis. Found multidimensional GP example.
    
    Generalize the BO argmax function to support N dimensions
    
    Support 3+ input dimensions (parameters)
    
    Refactor argmax, much easier method already provided by a module helper function
    
    Return both the known argmax and potential argmax
    
    Refactored function definition
    
    Logging progress during optimization
    
    Write results to file
    
    Parse previously computed samples
    
    Append new params and output to params output file
    
    Refactor xx and yy to lists, not dicts
    
    Refactor interface to BO argmax, uses lists instead of dict
    
    Cleaner BO interface, ordereddict
    
    Refactor returned values as ordereddict
    
    Log previously computed samples
    
    Default to zero sample iterations (only compute from current samples)
    
    Default settings.json file for easy configuration
    
    Use json settings to configure the analysis
    
    Move optimization code into new folder
    
    Rename latin hypercube sampling to LHS (from LHC)
    
    README, refactored to use pandas for utility parsing, and general clean up
    
    Markdown for README
    
    More markdown for readme
    
    Add introduction to readme
    
    More readme editing
    
    Add Grid Search feature. Now able to sample parameter space uniformly in an N-dimensional grid.
    
    Add description of grid search. General cleanup.

---
+++ scripts/parameter_analysis/parameter_optimization/multidimGP_plot_example.py
@@ -0,0 +1,155 @@
+from bayes_opt import BayesianOptimization
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib import mlab
+from matplotlib import gridspec
+
+
+# https://github.com/fmfn/BayesianOptimization/issues/18
+
+def unique_rows(a):
+    """
+    A functions to trim repeated rows that may appear when optimizing.
+    This is necessary to avoid the sklearn GP object from breaking
+
+    :param a: array to trim repeated rows from
+
+    :return: mask of unique rows
+    """
+
+    # Sort array and kep track of where things should go back to
+    order = np.lexsort(a.T)
+    reorder = np.argsort(order)
+
+    a = a[order]
+    diff = np.diff(a, axis=0)
+    ui = np.ones(len(a), 'bool')
+    ui[1:] = (diff != 0).any(axis=1)
+
+    return ui[reorder]
+
+
+
+def target(x, y):
+    a = np.exp(-( (x - 2)**2/0.7 + (y - 4)**2/1.2) + (x - 2)*(y - 4)/1.6 )
+    b = np.exp(-( (x - 4)**2/3 + (y - 2)**2/2.) )
+    c = np.exp(-( (x - 4)**2/0.5 + (y - 4)**2/0.5) + (x - 4)*(y - 4)/0.5 )
+    d = np.sin(3.1415 * x)
+    e = np.exp(-( (x - 5.5)**2/0.5 + (y - 5.5)**2/.5) )
+    return 2*a + b - c + 0.17 * d + 2*e
+
+
+
+n = 1e5
+x = np.linspace(0, 6, 300)
+y = np.linspace(0, 8, 300)
+X, Y = np.meshgrid(x, y)
+x = X.ravel()
+y = Y.ravel()
+X = np.vstack([x, y]).T[:, [1, 0]]
+z = target(x, y)
+
+print X, X.shape
+
+print(max(z))
+print(min(z))
+
+
+
+fig, axis = plt.subplots(1, 1, figsize=(14, 10))
+gridsize=150
+
+im = axis.hexbin(x, y, C=z, gridsize=gridsize, cmap=cm.jet, bins=None, vmin=-0.9, vmax=2.1)
+axis.axis([x.min(), x.max(), y.min(), y.max()])
+
+cb = fig.colorbar(im, )
+cb.set_label('Value')
+
+
+
+def posterior(bo, X):
+    ur = unique_rows(bo.X)
+    bo.gp.fit(bo.X[ur], bo.Y[ur])
+    mu, sigma2 = bo.gp.predict(X, return_std=True)
+    return mu, np.sqrt(sigma2), bo.util.utility(X, bo.gp, bo.Y.max())
+
+def plot_2d(name=None):
+
+    mu, s, ut = posterior(bo, X)
+
+    fig, ax = plt.subplots(2, 2, figsize=(14, 10))
+    gridsize=150
+
+    # fig.suptitle('Bayesian Optimization in Action', fontdict={'size':30})
+
+    # GP regression output
+    ax[0][0].set_title('Gausian Process Predicted Mean', fontdict={'size':15})
+    im00 = ax[0][0].hexbin(x, y, C=mu, gridsize=gridsize, cmap=cm.jet, bins=None, vmin=-0.9, vmax=2.1)
+    ax[0][0].axis([x.min(), x.max(), y.min(), y.max()])
+    ax[0][0].plot(bo.X[:, 1], bo.X[:, 0], 'D', markersize=4, color='k', label='Observations')
+
+    ax[0][1].set_title('Target Function', fontdict={'size':15})
+    im10 = ax[0][1].hexbin(x, y, C=z, gridsize=gridsize, cmap=cm.jet, bins=None, vmin=-0.9, vmax=2.1)
+    ax[0][1].axis([x.min(), x.max(), y.min(), y.max()])
+    ax[0][1].plot(bo.X[:, 1], bo.X[:, 0], 'D', markersize=4, color='k')
+
+
+    ax[1][0].set_title('Gausian Process Variance', fontdict={'size':15})
+    im01 = ax[1][0].hexbin(x, y, C=s, gridsize=gridsize, cmap=cm.jet, bins=None, vmin=0, vmax=1)
+    ax[1][0].axis([x.min(), x.max(), y.min(), y.max()])
+
+    ax[1][1].set_title('Acquisition Function', fontdict={'size':15})
+    im11 = ax[1][1].hexbin(x, y, C=ut, gridsize=gridsize, cmap=cm.jet, bins=None, vmin=0, vmax=8)
+
+    np.where(ut.reshape((300, 300)) == ut.max())[0]
+    np.where(ut.reshape((300, 300)) == ut.max())[1]
+
+    ax[1][1].plot([np.where(ut.reshape((300, 300)) == ut.max())[1]/50.,
+                   np.where(ut.reshape((300, 300)) == ut.max())[1]/50.],
+                  [0, 6],
+                  'k-', lw=2, color='k')
+
+    ax[1][1].plot([0, 6],
+                  [np.where(ut.reshape((300, 300)) == ut.max())[0]/50.,
+                   np.where(ut.reshape((300, 300)) == ut.max())[0]/50.],
+                  'k-', lw=2, color='k')
+
+    ax[1][1].axis([x.min(), x.max(), y.min(), y.max()])
+
+    for im, axis in zip([im00, im10, im01, im11], ax.flatten()):
+        cb = fig.colorbar(im, ax=axis)
+        # cb.set_label('Value')
+
+    if name is None:
+        name = '_'
+
+    plt.tight_layout()
+
+    # Save or show figure?
+    # fig.savefig('bo_eg_' + name + '.png')
+    plt.show()
+    plt.close(fig)
+
+
+
+bo = BayesianOptimization(target, {'x': (0, 6), 'y': (0, 8)})
+
+
+
+# gp_params = {'corr': 'absolute_exponential'}#, 'nugget': 1e-9}
+bo.maximize(init_points=5, n_iter=0, acq='ucb', kappa=10)
+
+
+
+plot_2d("{:03}".format(len(bo.X)))
+
+
+
+# Turn interactive plotting off
+plt.ioff()
+
+for i in range(50):
+    bo.maximize(init_points=0, n_iter=1, acq='ucb', kappa=10, **gp_params)
+    plot_2d("{:03}".format(len(bo.X)))
---
+++ scripts/parameter_analysis/parameter_optimization/parameter_optimizer.py
@@ -0,0 +1,172 @@
+from mako.template import Template
+from mako.runtime import Context
+from StringIO import StringIO
+import os
+from settings_parser import *
+import subprocess
+import numpy as np
+from collections import OrderedDict
+from bayesian_optimization import BayesianOptimizeArgmax
+import json
+import os.path
+import logging
+import threading
+import time
+import os
+import signal
+import sys
+import xml.etree.ElementTree as ET
+
+import queue
+from concurrent import futures
+
+
+
+def generateMissionFile(templateFullFilename, parameterLabels, parameterValues, logPath, missionIteration):
+    print parameterLabels, parameterValues
+
+    if len(parameterLabels) != len(parameterValues):
+        raise ValueError('Parameter Labels and Values are mismatched. (Labels=', len(parameterLabels), 'values=', len(parameterValues))
+        quit()
+
+    # Load the mission file
+    myTemplate = Template(filename=templateFullFilename)
+
+    try:
+        # Add log path
+        parameters = dict(zip(parameterLabels + ['log_path'], parameterValues + [logPath + 'iter-' + str(missionIteration)]))
+
+        # Parse in the params
+        missionData = myTemplate.render(**parameters)
+
+        # Save the file
+        folder = os.path.dirname(os.path.abspath(templateFullFilename))
+        filename = os.path.splitext(os.path.basename(templateFullFilename))[0]
+        fullFilePath = folder + '/' + filename + str(missionIteration) + '.xml'
+        file = open(fullFilePath, 'w')
+        file.write(missionData)
+        file.close()
+
+        return fullFilePath
+    except NameError:
+        print 'Detected incorrect or missing parameters in mission xml.'
+        quit()
+
+# Return xx and yy parsed from file
+def parseSamples(file):
+    xx = []
+    yy = []
+    if os.path.isfile(file):
+        with open(file) as f:
+            f.readline() # skip the header
+
+            for iter, line in enumerate(f):
+                values = line.strip().split('=')
+                print values
+                xx.append([float(i) for i in values[0].split(',')])
+                yy.append(float(values[1]))
+    return xx, yy
+
+def saveSamples(file, xx, yy, header):
+    # Write header if first time writing to file (it's a new file)
+    if not os.path.isfile(file):
+        with open(file, "a") as myfile:
+            myfile.write(",".join((str(x) for x in header)) + '\n')
+
+    # Write the known data points
+    with open(file, "a") as myfile:
+        myfile.write(",".join((str(x) for x in xx)) + '=' + str(yy) + '\n')
+
+
+# postScrimmageAnalysis gets called on a directory of mission files, not a specific mission, returning an output value
+# ranges is a dict of tuple ranges keyed by param name
+def optimize(templateFilename, ranges, stateSpaceSampler,
+             postScrimmageAnalysis, functionApproximator, logPath,
+             numExploreSamples=0, numIterationsPerSample=1, numExploitSamples=0):
+    folder = os.path.dirname(os.path.abspath(templateFilename))
+    logName = os.path.splitext(os.path.basename(templateFilename))[0]
+    samplesFile = folder + '/' + logName + '_samples.log'
+    logFile = folder + '/' + logName + '.log'
+    logging.basicConfig(filename=logFile, filemode='w', level=logging.DEBUG)
+
+    xx, yy = parseSamples(samplesFile)
+    if len(xx) > 0 and len(yy) > 0:
+        logging.info('Samples from file'+samplesFile)
+        logging.info('Initial xx: ' + ','.join((str(x) for x in xx)))
+        logging.info('Initial yy: ' + ','.join((str(x) for x in yy)))
+
+    # Initial new exploration parameters
+    logging.info('Sampling State Space')
+    new_xx = stateSpaceSampler(ranges, numExploreSamples)
+    simulationIter = len(xx)
+
+    for loopIter in range(numExploitSamples+1):
+        newBatchStartingIter = simulationIter
+        for params in new_xx:
+            # Parse sample into template mission
+            logging.info('Generating Mission File with params: ' + ','.join((str(x) for x in zip(ranges.keys(),params))))
+            missionFile = generateMissionFile(templateFilename, ranges.keys(), params, logPath, simulationIter)
+
+            xx.append(params)
+
+            # Run the same params multiple times (in order to get a better average result)
+            scrimmageProcesses = []
+            for paramScrimmageIter in range(numIterationsPerSample):
+                # run scrimmage
+                logging.info('Executing Mission File')
+                scrimmageProcesses.append(subprocess.Popen(["scrimmage", missionFile]))
+
+            # Wait for all processes to finish
+            for process in scrimmageProcesses:
+                process.wait()
+
+            os.remove(missionFile)
+            logging.info('Completed Scrimmage Simulations')
+
+            simulationIter+=1
+
+        # analysis for all mission results
+        logging.info('Parsing Results')
+        for iter in range(newBatchStartingIter,simulationIter):
+            yy.append(postScrimmageAnalysis(logPath+'iter-'+str(iter)))
+
+            # Append the new params and output
+            saveSamples(samplesFile, xx[iter], yy[iter], ranges.keys())
+
+        # Use f_hat to guess some optimal params
+        logging.info('Approximating function')
+        knownArgmax, expectedValue, nextArgmax = functionApproximator(xx, yy, ranges)
+        logging.debug('knownArgmax: ' + json.dumps(knownArgmax))
+        logging.debug('expectedValue: ' + json.dumps(expectedValue))
+        logging.debug('nextArgmax: ' + json.dumps(nextArgmax))
+
+        new_xx = [nextArgmax.values()]
+    logging.info('Optimization complete.')
+    return knownArgmax, expectedValue
+
+if __name__ == "__main__":
+    # Parse the configuration from the settings file
+    parser = SettingsParser('settings.json')
+    missionFile = parser.getMissionFile()
+    logPath = parser.getLogPath()
+    sampler = parser.getStateSpaceSampler()
+    postMissionAnalyzer = parser.getPostMissionAnalyzer()
+    fApprox = parser.getFunctionApproximator()
+    numExploreSamples = parser.getNumExploreSamples()
+    numIterationsPerSample = parser.getNumIterationsPerSample()
+    numExploitSamples = parser.getNumExploitSamples()
+    ranges = parser.getRanges()
+
+    knownArgmax, expectedValue = optimize(
+        missionFile,
+        ranges,
+        sampler,
+        postMissionAnalyzer,
+        fApprox,
+        logPath,
+        numExploreSamples=numExploreSamples,
+        numIterationsPerSample=numIterationsPerSample,
+        numExploitSamples=numExploitSamples)
+
+    print 'Known Argmax:', knownArgmax
+    print 'Expected Value:', expectedValue
