d03f0aaef1f45cf0ebf12b44de1c5b3f68e872f5
Bogdan Vlahov <bogdan.vlahov@gtri.gatech.edu>
2017-10-31 16:16:48 -0400

    Actually added new files for OpenAI Tutorial

---
+++ include/scrimmage/plugins/autonomy/TutorialOpenAIAutonomy/TutorialOpenAIAutonomy.h
@@ -0,0 +1,54 @@
+/*!
+ * @file
+ *
+ * @section LICENSE
+ *
+ * Copyright (C) 2017 by the Georgia Tech Research Institute (GTRI)
+ *
+ * This file is part of SCRIMMAGE.
+ *
+ *   SCRIMMAGE is free software: you can redistribute it and/or modify it under
+ *   the terms of the GNU Lesser General Public License as published by the
+ *   Free Software Foundation, either version 3 of the License, or (at your
+ *   option) any later version.
+ *
+ *   SCRIMMAGE is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ *   License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with SCRIMMAGE.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
+ * @author Eric Squires <eric.squires@gtri.gatech.edu>
+ * @date 31 July 2017
+ * @version 0.1.0
+ * @brief Brief file description.
+ * @section DESCRIPTION
+ * A Long description goes here.
+ *
+ */
+
+#ifndef TutorialOpenAIAutonomy_H_
+#define TutorialOpenAIAutonomy_H_
+
+#include <scrimmage/plugins/autonomy/ExternalControl/ExternalControl.h>
+
+#include <map>
+#include <string>
+
+class TutorialOpenAIAutonomy : public ExternalControl {
+ public:
+    virtual void init(std::map<std::string, std::string> &params);
+    double calc_reward(double time);
+
+ protected:
+ 	double radius_;
+    virtual bool handle_action(
+        double t, double dt, const scrimmage_proto::Action &action);
+    virtual scrimmage_proto::SpaceParams action_space_params();
+
+};
+
+#endif
---
+++ include/scrimmage/plugins/sensor/TutorialOpenAISensor/TutorialOpenAISensor.h
@@ -0,0 +1,59 @@
+/*!
+ * @file
+ *
+ * @section LICENSE
+ *
+ * Copyright (C) 2017 by the Georgia Tech Research Institute (GTRI)
+ *
+ * This file is part of SCRIMMAGE.
+ *
+ *   SCRIMMAGE is free software: you can redistribute it and/or modify it under
+ *   the terms of the GNU Lesser General Public License as published by the
+ *   Free Software Foundation, either version 3 of the License, or (at your
+ *   option) any later version.
+ *
+ *   SCRIMMAGE is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ *   License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with SCRIMMAGE.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
+ * @author Eric Squires <eric.squires@gtri.gatech.edu>
+ * @date 31 July 2017
+ * @version 0.1.0
+ * @brief Brief file description.
+ * @section DESCRIPTION
+ * A Long description goes here.
+ *
+ */
+
+#ifndef TutorialOpenAISensor_H_
+#define TutorialOpenAISensor_H_
+
+
+#include <scrimmage/sensor/Sensor.h>
+
+#include <map>
+#include <string>
+#include <vector>
+
+namespace boost {
+template <class T> class optional;
+}
+
+namespace scrimmage_proto {
+class SpaceParams;
+class SpaceSample;
+}
+
+class TutorialOpenAISensor : public scrimmage::Sensor {
+public:
+    virtual boost::optional<scrimmage_proto::SpaceParams> observation_space_params();
+    virtual boost::optional<scrimmage::MessagePtr<scrimmage_proto::SpaceSample>>
+        sensor_msg_flat(double t);
+};
+
+#endif
