7db314c3a0b93237fe782e56e8378f6eeeca7096
Bogdan Vlahov <bogdan.vlahov@gtri.gatech.edu>
2017-11-10 16:07:12 -0500

    Remove time step passthrough

--- python/scrimmage/external_control.py
+++ python/scrimmage/external_control.py
@@ -170,11 +170,6 @@ class ScrimmageEnv(gym.Env):
         run_node.attrib['enable_gui'] = str(enable_gui)
         if not bool(enable_gui):
             run_node.attrib['time_warp'] = "0"
-
-        # get number of time steps
-        dt = float(run_node.attrib['dt'])
-        total_time = float(run_node.attrib['end'])
-        self.num_steps = int(total_time/dt + 1);
         
         # logging
         log_node = root.find('log_dir')
