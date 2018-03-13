bab25063532963f1da23f1bc903d207b0bb6478e
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2018-03-13 16:57:18 -0400

    testing python autonomy

--- python/scrimmage/__init__.py
+++ python/scrimmage/__init__.py
@@ -32,9 +32,9 @@
 from scrimmage.bindings import *
 from scrimmage.utils import *
 
-try:
-    from scrimmage.external_control import ScrimmageEnv
-except ImportError:
-    print('unable to import ScrimmageEnv so the openAI environment will '
-          'be disabled (this occurs when either openAI gym or GRPC '
-          'are not installed)')
+#try:
+#    from scrimmage.external_control import ScrimmageEnv
+#except ImportError:
+#    print('unable to import ScrimmageEnv so the openAI environment will '
+#          'be disabled (this occurs when either openAI gym or GRPC '
+#          'are not installed)')
