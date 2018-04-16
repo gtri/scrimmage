bc105013c7b46f4a5acff96960e0965203343246
esquires <eric.squires@gtri.gatech.edu>
2018-04-16 08:51:43 -0400

    fix formatting in Boids.h

--- include/scrimmage/plugins/autonomy/Boids/Boids.h
+++ include/scrimmage/plugins/autonomy/Boids/Boids.h
@@ -42,38 +42,38 @@ namespace scrimmage {
 namespace autonomy {
 class Boids : public scrimmage::Autonomy{
  public:
-     virtual void init(std::map<std::string, std::string> &params);
-     virtual bool step_autonomy(double t, double dt);
+    virtual void init(std::map<std::string, std::string> &params);
+    virtual bool step_autonomy(double t, double dt);
 
  protected:
-     void velocity_controller(Eigen::Vector3d &v);
+    void velocity_controller(Eigen::Vector3d &v);
 
-     bool show_shapes_;
-     double max_speed_;
+    bool show_shapes_;
+    double max_speed_;
 
-     double w_align_;
-     double w_avoid_team_;
-     double w_centroid_;
-     double w_avoid_nonteam_;
-     double w_goal_;
+    double w_align_;
+    double w_avoid_team_;
+    double w_centroid_;
+    double w_avoid_nonteam_;
+    double w_goal_;
 
-     double fov_el_;
-     double fov_az_;
-     double comms_range_;
-     double minimum_team_range_;
-     double minimum_nonteam_range_;
-     double sphere_of_influence_;
+    double fov_el_;
+    double fov_az_;
+    double comms_range_;
+    double minimum_team_range_;
+    double minimum_nonteam_range_;
+    double sphere_of_influence_;
 
-     Eigen::Vector3d goal_;
+    Eigen::Vector3d goal_;
 
-     // variable io
-     int io_vel_x_idx_ = 0;
-     int io_vel_y_idx_ = 0;
-     int io_vel_z_idx_ = 0;
+    // variable io
+    int io_vel_x_idx_ = 0;
+    int io_vel_y_idx_ = 0;
+    int io_vel_z_idx_ = 0;
 
-     int io_vel_idx_ = 0;
-     int io_turn_rate_idx_ = 0;
-     int io_pitch_rate_idx_ = 0;
+    int io_vel_idx_ = 0;
+    int io_turn_rate_idx_ = 0;
+    int io_pitch_rate_idx_ = 0;
 };
 } // namespace autonomy
 } // namespace scrimmage
