320cc72324acec3dc68790058969e819f047d420
Laura Strickland <laurathepluralized@gmail.com>
2018-06-05 18:44:17 -0400

    minor grammar and spelling fixes

--- cmake/Modules/distclean.cmake
+++ cmake/Modules/distclean.cmake
@@ -1,5 +1,5 @@
 # This CMake script will delete build directories and files to bring the
-# package back to it's distribution state
+# package back to its distribution state
 
 # Loop over the directories and delete each one
 string(REPLACE " " ";" DIRS_TO_REMOVE ${DIRS_TO_REMOVE})
