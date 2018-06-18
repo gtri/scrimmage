ee4103fa757d1b170456bdecb09a72c40ce63401
Christopher Richardson <christopher.richardson@gtri.gatech.edu>
2018-06-18 11:14:24 -0400

    add origin offset param to terrain

--- src/parse/MissionParse.cpp
+++ src/parse/MissionParse.cpp
@@ -731,9 +731,23 @@ bool MissionParse::parse_terrain() {
         if (((northp && boost::to_upper_copy(terrain_parse.params()["hemisphere"]) == "NORTH") ||
              (!northp && boost::to_upper_copy(terrain_parse.params()["hemisphere"]) == "SOUTH")) &&
             zone == get("zone", terrain_parse.params(), -2)) {
+            
+            double origin_offset_x = 0;
+            double origin_offset_y = 0;
+            double origin_offset_z = 0;
 
-            utm_terrain_->set_x_translate(x_easting);
-            utm_terrain_->set_y_translate(y_northing);
+            if (   terrain_parse.params().count("origin_offset_x") == 1
+                && terrain_parse.params().count("origin_offset_y") == 1
+                && terrain_parse.params().count("origin_offset_z") == 1) {
+
+                origin_offset_x = stod(terrain_parse.params()["origin_offset_x"]);
+                origin_offset_y = stod(terrain_parse.params()["origin_offset_y"]);
+                origin_offset_z = stod(terrain_parse.params()["origin_offset_z"]);
+            }
+
+            utm_terrain_->set_x_translate(x_easting - origin_offset_x);
+            utm_terrain_->set_y_translate(y_northing - origin_offset_y);
+            utm_terrain_->set_z_translate(altitude_origin() - origin_offset_z);
             utm_terrain_->set_z_translate(altitude_origin());
             utm_terrain_->set_system(terrain_parse.params()["system"]);
             utm_terrain_->set_zone(zone);
