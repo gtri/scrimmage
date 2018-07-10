36c192d4e8d25b9e035a7e7a6ec834b64a6d254f
esquires <eric.squires@gtri.gatech.edu>
2018-07-10 10:06:51 -0400

    use override rather than virtual in autonomy PluginTemplate.h

--- scripts/templates/autonomy/PluginTemplate.h
+++ scripts/templates/autonomy/PluginTemplate.h
@@ -42,8 +42,8 @@ namespace autonomy {
 class (>>>PLUGIN_NAME<<<) : public scrimmage::Autonomy {
  public:
     (>>>PLUGIN_NAME<<<)();
-    virtual void init(std::map<std::string, std::string> &params);
-    virtual bool step_autonomy(double t, double dt);
+    void init(std::map<std::string, std::string> &params) override;
+    bool step_autonomy(double t, double dt) override;
 
  protected:
     int follow_id_;
