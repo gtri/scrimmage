gtri/scrimmage

commit fe381c8246e4e3db3df57c3eedd7f24a1c2c5f35

Added frames2csv, utilities docs

python/scrimmage/__init__.py
@@ -28,3 +28,4 @@
 # @section DESCRIPTION
 # A Long description goes here.
 #
+from .proto_utils import read_frames

python/scrimmage/utils.py
@@ -42,6 +42,7 @@ try:
 except ImportError:
     pass
 
+from scrimmage.bindings import frames2pandas
 
 def find_mission(fname):
     """Return the filename with path of a given mission file.
@@ -249,3 +250,19 @@ def make_dirs_recursive(path):
     for j in range(idx, len(components) + 1):
         os.mkdir(os.sep.join(components[:j]))
 
+def downsample_frames(frames_file, new_dt=None):
+    df = frames2pandas(frames_file)
+    if new_dt is not None:
+        try:
+            dt = float(df['time'][1] - df['time'][0])
+            print("Inferred dt from frames file: %f" % dt)
+        except:
+            print("Warning: failed to infer dt")
+            dt = 1.0
+        return df.iloc[0::int(new_dt/dt), :]
+    else:
+        return df
+
+def frames2csv(frames_file, new_dt=None, out_filename='./frames.csv'):
+    df = downsample_frames(frames_file, new_dt)
+    df.to_csv(out_filename)

commit dc3e34e336e7f54e045de2d94450471089e121fc

fix openai test
fixes issue introduced in fe381c8.

python/scrimmage/__init__.py
@@ -28,4 +28,3 @@
 # @section DESCRIPTION
 # A Long description goes here.
 #
-from .proto_utils import read_frames

test/CMakeLists.txt
@@ -28,9 +28,6 @@ else()
     endif()
 endif()
 
-# TODO: disable openai until segfault fixed...
-LIST(REMOVE_ITEM test_files "test_openai.cpp")
-
 foreach(test_file ${test_files})
   get_filename_component(test_name ${test_file} NAME_WE)
   add_executable(${test_name} ${test_file})