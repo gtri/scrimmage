f85917af4bc4406ca1c2e8f41eb59e5cd7dd6785
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2019-04-15 15:56:28 -0400

    In 6DOF sensor, dynamic pointer cast after init()

--- include/scrimmage/plugins/sensor/RigidBody6DOFStateSensor/RigidBody6DOFStateSensor.h
+++ include/scrimmage/plugins/sensor/RigidBody6DOFStateSensor/RigidBody6DOFStateSensor.h
@@ -56,7 +56,7 @@ class RigidBody6DOFStateSensor : public scrimmage::Sensor {
  protected:
     std::shared_ptr<std::default_random_engine> gener_;
     std::vector<std::shared_ptr<std::normal_distribution<double>>> pos_noise_;
-    std::shared_ptr<scrimmage::motion::RigidBody6DOFBase> motion_;
+    std::shared_ptr<scrimmage::motion::RigidBody6DOFBase> motion_ = nullptr;
 
     PublisherPtr pub_;
 
