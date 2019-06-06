7ad3929226ccfe8a41ba772904aa5e6f267357ab
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2019-06-06 19:47:22 -0400

    Allow clamping of PID output, access PID member variables

--- src/common/PID.cpp
+++ src/common/PID.cpp
@@ -38,11 +38,16 @@
 #include <vector>
 
 #include <boost/algorithm/string.hpp>
+#include <boost/algorithm/clamp.hpp>
+
+using boost::algorithm::clamp;
 
 namespace scrimmage {
 
 PID::PID() : kp_(0), ki_(0), kd_(0), prev_error_(0), integral_(0),
-             setpoint_(0), integral_band_(0), is_angle_(false) {}
+             setpoint_(0), integral_band_(0), is_angle_(false),
+             derivative_(0), error_(0), output_min_(0), output_max_(0),
+             enable_output_limits_(false) {}
 
 bool PID::init(const std::string &str, const bool &is_angle) {
     std::vector<std::string> str_vals;
@@ -68,38 +73,55 @@ bool PID::init(const std::string &str, const bool &is_angle) {
     return true;
 }
 
-void PID::set_parameters(double kp, double ki, double kd) {
+void PID::set_output_limits(const double &min, const double &max,
+                            const double &enable) {
+    output_min_ = min;
+    output_max_ = max;
+    enable_output_limits_ = enable;
+}
+
+void PID::set_parameters(const double &kp, const double &ki, const double &kd) {
     kp_ = kp;
     ki_ = ki;
     kd_ = kd;
 }
 
-void PID::set_setpoint(double setpoint) {
+void PID::set_setpoint(const double &setpoint) {
     setpoint_ = setpoint;
 }
 
-void PID::set_integral_band(double integral_band) {
+void PID::set_integral_band(const double &integral_band) {
     integral_band_ = integral_band;
 }
 
-void PID::set_is_angle(bool is_angle) { is_angle_ = is_angle; }
+void PID::set_is_angle(const bool &is_angle) { is_angle_ = is_angle; }
 
-double PID::step(double dt, double measurement) {
+const double & PID::setpoint() { return setpoint_; }
+const double & PID::kp() { return kp_; }
+const double & PID::ki() { return ki_; }
+const double & PID::kd() { return kd_; }
+const double & PID::integral_band() { return integral_band_; }
+const double & PID::integral() { return integral_; }
+const double & PID::error() { return error_; }
+const double & PID::previous_error() { return prev_error_; }
+const double & PID::derivative() { return derivative_; }
 
-    double error = setpoint_ - measurement;
+double PID::step(const double &dt, const double &measurement) {
+    error_ = setpoint_ - measurement;
     if (is_angle_) {
-        error = Angles::angle_pi(error);
+        error_ = Angles::angle_pi(error_);
     }
 
-    if (std::abs(error) > integral_band_) {
+    if (std::abs(error_) > integral_band_) {
         integral_ = 0;
     } else {
-        integral_ += error * dt;
+        integral_ += error_ * dt;
     }
 
-    double derivative = (error - prev_error_) / std::max(1.0e-9, dt);
-    double u = kp_*error + ki_*integral_ + kd_*derivative;
-    prev_error_ = error;
-    return u;
+    derivative_ = (error_ - prev_error_) / std::max(1.0e-9, dt);
+    double u = kp_*error_ + ki_*integral_ + kd_*derivative_;
+    prev_error_ = error_;
+
+    return enable_output_limits_ ? clamp(u, output_min_, output_max_) : u;
 }
 } // namespace scrimmage
