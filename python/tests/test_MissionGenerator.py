68bb8e9289dff2a201450d5cc817df0801b9f5d2
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2020-03-06 12:50:12 -0500

    added mission templates

--- python/tests/test_MissionGenerator.py
+++ python/tests/test_MissionGenerator.py
@@ -1,8 +1,17 @@
 import unittest
 
+import scrimmage.mission.MissionGenerator as MG
+
 class TestMissionGenerator(unittest.TestCase):
     def test_mission_gen(self):
-        self.assertEqual(True, True)
+
+        script_path = os.path.dirname(os.path.realpath(__file__))
+        mission_yaml_path = os.path.join(script_path, "templates/mission.yaml")
+
+        mg = MG.MissionGenerator(mission_yaml_path)
+        print(mg.mission)
+        self.assertTrue(False)
+
 
 if __name__ == '__main__':
     unittest.main()
