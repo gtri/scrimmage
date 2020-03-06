746fad85a45ee27e114bdbd91921737d8bd71e45
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2020-03-06 12:06:14 -0500

    created MissionGenerator class

---
+++ python/tests/test_MissionGenerator.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestMissionGenerator(unittest.TestCase):
+    def test_mission_gen(self):
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest.main()
