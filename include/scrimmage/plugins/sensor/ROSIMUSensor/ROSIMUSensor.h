2b8d944148f0a92928a55a2da3cec67baa464950
David Burke <David.Burke@gtri.gatech.edu>
2020-03-16 17:14:59 -0400

    -ported IMU data code from Matlab to c++ -updated csv and ros message outputs to new values -added a bunch of supporting functions for converting between coordinate frames

--- include/scrimmage/plugins/sensor/ROSIMUSensor/ROSIMUSensor.h
+++ include/scrimmage/plugins/sensor/ROSIMUSensor/ROSIMUSensor.h
@@ -51,21 +51,56 @@ namespace sensor {
 
 class ROSIMUSensor : public scrimmage::Sensor {
  public:
-    ROSIMUSensor();
-    void init(std::map<std::string, std::string> &params) override;
-    bool step() override;
-    void close(double t) override;
+  ROSIMUSensor();
+  void init(std::map<std::string, std::string> &params) override;
+  bool step() override;
+  void close(double t) override;
 
  protected:
-    std::string ros_namespace_;
-    std::shared_ptr<ros::NodeHandle> nh_;
-    ros::Publisher imu_pub_;
+  std::string ros_namespace_;
+  std::shared_ptr<ros::NodeHandle> nh_;
+  ros::Publisher imu_pub_;
+  double prev_time_ = 0.0;
+  scrimmage::CSV csv;
 
-    Eigen::Vector3d prev_vel_;
-    Quaternion prev_quat_;
-    double prev_time_ = 0.0;
+  // WGS84 constants
+  const double ecc = 8.1819190842621E-2;
+  const double earth_radius = 6378137;
+  const double flattening_inverse = 298.257223563;
+  const double e_prime = sqrt(1 - ecc * ecc);
+  const double ae_squared = earth_radius * ecc * ecc;
+  const double k1 = 1 - ecc * ecc;
+  const double earth_semiminor_axis = earth_radius * (1 - 1 / flattening_inverse); // aprox. 6356752.3142;
+  const double a2 = earth_radius * earth_radius;
+  const double b2 = earth_semiminor_axis * earth_semiminor_axis;
+  const double e = 521854.0089737218;
+  const double e2 = 272331606681.94534;
+  const double wgs84_grav = 3986004.418e+08;
+  const double earth_rate = 7.2921151467e-5;
+  const double omega2 = earth_rate * earth_rate;
 
-    scrimmage::CSV csv;
+  // storage variables for previous frame data
+  Eigen::Vector3d vel_t1;
+  Eigen::Vector3d pos_ECEF_t1;
+  Eigen::Matrix3d c_ECEF_To_NED_t1;
+  Eigen::Quaterniond prev_qBody_to_ECEF;
+  Eigen::Quaterniond qBody_to_ECEF_hat;
+  Eigen::Vector3d p_hat;
+  Eigen::Vector3d v_hat;
+  bool first_sample_collected = false;
+  Eigen::Vector3d lla_to_ecef(double lat, double lon, double alt);
+  Eigen::Matrix3d enu_to_ecef_rotation(double lat, double lon);
+  Eigen::Matrix3d ned_to_ecef_rotation(double lat, double lon);
+  Eigen::Vector3d get_deltaV(Eigen::Vector3d pos, Eigen::Vector3d vel, Eigen::Quaterniond bodyToEcef, double deltaT);
+  Eigen::Vector3d ecef_to_lla(Eigen::Vector3d ecef);
+  Eigen::Matrix3d get_rotation_matrix_from_ecef_to_ned(double lat, double lon);
+  Eigen::Vector3d gravity_ned_from_lla(Eigen::Vector3d lla);
+  Eigen::Matrix3d skew_sym(Eigen::Vector3d vector);
+  Eigen::Vector3d get_delta_theta(Eigen::Quaterniond qBodyToECEFt1Hat, Eigen::Quaterniond qBodyToECEFt2, double inertialDeltaT);
+  Eigen::Vector3d inv_skew_sym(Eigen::Matrix3d inputMatrix);
+  Eigen::Quaterniond propagate_quaternion(Eigen::Quaterniond qBodyToECEFt1Hat, Eigen::Vector3d deltaThetaBodyWRTInertialInBody, double inertialDeltaT);
+  Eigen::Quaterniond integrate_quaternion(Eigen::Quaterniond qBToA, Eigen::Vector3d deltaThetasBFrame);
+  Eigen::Quaterniond omega_to_q_dot(Eigen::Quaterniond qBToA, Eigen::Vector3d omegaABInB);
 
  private:
 };
